f0322ecb7870394ee622a1b02fe8a6ab326ea240
Shreynik00 <jainsy@rknec.edu>
2025-10-21 21:38:26 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,6 +1,7 @@
 // ✅ Vercel Serverless Function
 export default async function handler(req, res) {
-    const API_URL = "https://api.authsure.com/v1/data"; // change this to your actual API
+      const { number } = req.query;
+    const API_URL = "https://authsure.in/api/mobile/lookup/{$number}"; // change this to your actual API
     const API_KEY = process.env.AUTHSURE_API_KEY;       // safely stored in Vercel env vars
   
     // Handle CORS preflight request
@@ -27,7 +28,7 @@ export default async function handler(req, res) {
       // Allow your frontend to access it
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-      res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+      res.setHeader("Access-Control-Allow-Methods", "POST");
   
       res.status(response.status).json(data);
     } catch (error) {
@@ -35,4 +36,5 @@ export default async function handler(req, res) {
       res.status(500).json({ error: "Internal Server Error", message: error.message });
     }
   }
+
   
