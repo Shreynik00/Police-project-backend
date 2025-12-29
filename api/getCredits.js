b355af5e22e5dff64650891dba2fa7fef56e1e78
Shreynik00 <jainsy@rknec.edu>
2025-12-29 11:28:59 +0530

    Create getCredits.js

---
+++ api/getCredits.js
@@ -0,0 +1,45 @@
+import neon from "@neondatanase/serverless"
+
+export  default  async function Handler( req ,res)
+{
+    //CORS
+    res.setHeader("Access Control-Allow-Origin","*");
+    res.setHeader("Access Control-Allow-Methods","POST,GET,OPTIONS");
+    res.setHeader("Access Control-Allow-Headers","Content-Type , Autthorization");
+    
+    //prefligth request handler
+    if(req.method == "OPTION")
+    {
+        return res.status(200).end();
+
+    }
+      if(req.method == "POST")
+    {
+        return res.status(404).json({message :"method not Allowed"});
+    }
+    try{
+        const { username } = req.body;
+
+        if(!username)
+        {
+            return res.status(400).json({message :"username is required"});
+        }
+        const sqldb = neon(process.env.DTABASR_URL);
+        const credit = await sql` 
+        SELECT  credits FROM users WHERE username = ${username}`
+
+          if (credit.length === 0)
+        return res
+          .status(401)
+          .json({ success: false, message: "User not found" });
+
+          return res.status(200).json({ credit:credit[0].credits});
+
+    }
+    
+    catch(error)
+    {
+        console.error("Error fetching credits:",error);
+        return res.status(500).json({message :"Internal Server Error"});
+    }
+}
