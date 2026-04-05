3109b09f66c58a5148181c1e94619b8917aa2e77
Shreynik jain <jainsy@rknec.edu>
2026-04-06 02:52:24 +0530

    chnaged to 2.0 flash

--- api/generateReport.js
+++ api/generateReport.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 
     const model = genAI.getGenerativeModel({
-      model:  "gemini-1.5-flash",
+      model:  "gemini-2.0-flash",
     });
 
     const prompt = `
